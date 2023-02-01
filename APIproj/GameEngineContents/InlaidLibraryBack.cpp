e41d41301dc6c5d7906ca44251a04e695a94a133
pasongsj <audrbekf@gmail.com>
2023-02-01 12:06:17 +0900

    주석정리

--- APIproj/GameEngineContents/InlaidLibraryBack.cpp
+++ APIproj/GameEngineContents/InlaidLibraryBack.cpp
@@ -23,59 +23,38 @@ void InlaidLibraryBack::Start()
 		BGSize = Render->GetImage()->GetImageScale();
 		IsRanderPos.insert(0);
 	}
-	//{
-	//	GameEngineRender* Render = CreateRender("InlaidLibraryStage.bmp", VSRenderOrder::BackGround);
-	//	//Render->SetPosition(GameEngineWindow::GetScreenSize().half());
-	//	float4 LPos = Render->GetImage()->GetImageScale().half();
-	//	LPos.x -= 4096;
-	//	Render->SetPosition(LPos);
-	//	//Render->SetScale(Render->GetImage()->GetImageScale());
-	//	Render->SetScaleToImage();
-	//}
+
 }
 
 void InlaidLibraryBack::Update(float _DeltaTime)
 {
-	int RandPos = 0;
 	int CamX = (GetLevel()->GetCameraPos()).x;
-
+	int RenderPos = (CamX / BGSize.x);
 	if (CamX < 0) {
-		RandPos -= 1;
+		RenderPos -= 1;
 	}
-	RandPos += (CamX / BGSize.x);
-
 	CamX = ((CamX % BGSize.ix()) + BGSize.ix()) % BGSize.ix();
 
 
-	if (CamX < +200 && IsRanderPos.find(RandPos-1) == IsRanderPos.end()) {
-		--RandPos;
+	if (CamX < +200 && 
+		IsRanderPos.find(RenderPos-1) == IsRanderPos.end()) 
+	{
 		GameEngineRender* Render = CreateRender("InlaidLibraryStage.bmp", VSRenderOrder::BackGround);
 		float4 LPos = Render->GetImage()->GetImageScale().half();
-		LPos.x += RandPos * BGSize.x;
+		LPos.x += --RenderPos * BGSize.x;
 		Render->SetPosition(LPos);
 		Render->SetScaleToImage();
-		IsRanderPos.insert(RandPos);
-		int a = 0;
+		IsRanderPos.insert(RenderPos);
 	}
-	else if (CamX > BGSize.ix()- GameEngineWindow::GetScreenSize().ix() - 200 && IsRanderPos.find(RandPos + 1) == IsRanderPos.end()) {
-		++RandPos;
+	else if (CamX > BGSize.ix()- GameEngineWindow::GetScreenSize().ix() - 200 && 
+			IsRanderPos.find(RenderPos + 1) == IsRanderPos.end()) 
+	{
 		GameEngineRender* Render = CreateRender("InlaidLibraryStage.bmp", VSRenderOrder::BackGround);
-		float4 LPos = Render->GetImage()->GetImageScale().half();
-		LPos.x += RandPos * BGSize.x;
-		Render->SetPosition(LPos);
+		float4 RPos = Render->GetImage()->GetImageScale().half();
+		RPos.x += ++RenderPos * BGSize.x;
+		Render->SetPosition(RPos);
 		Render->SetScaleToImage();
-		IsRanderPos.insert(RandPos);
-		int a = 0;
+		IsRanderPos.insert(RenderPos);
 	}
-	//if ((GetLevel()->GetCameraPos()).x < 0.0f /*|| (GetLevel()->GetCameraPos()).x + GameEngineWindow::GetScreenSize().x > 4096.0f*/) {
-	//	int a = 0;
-	//}
-	//if ((GetLevel()->GetCameraPos()).x + GameEngineWindow::GetScreenSize().x > 4096.0f) {
-	//	int a = 0;
-	//}
-
-	/*if (abs(2048.0f - (GetLevel()->GetCameraPos()).x + GameEngineWindow::GetScreenSize().half().x) < GameEngineWindow::GetScreenSize().half().x) {
-		int a = 0;
-	}*/
 
 }
