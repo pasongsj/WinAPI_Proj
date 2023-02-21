e40ce6a27402c6724d6ec4df644647fefaa5fb88
pasongsj <audrbekf@gmail.com>
2023-02-22 01:30:59 +0900

    Random업데이트

---
+++ APIproj/GameEngineBase/GameEngineRandom.cpp
@@ -0,0 +1,16 @@
+#include "GameEngineRandom.h"
+#include <chrono>
+
+
+GameEngineRandom GameEngineRandom::MainRandom;
+
+GameEngineRandom::GameEngineRandom()
+	: MtGen(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))
+{
+
+}
+
+GameEngineRandom::~GameEngineRandom()
+{
+}
+
---
+++ APIproj/GameEngineBase/GameEngineRandom.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <random>
+
+// 설명 :
+class GameEngineRandom
+{
+
+public:
+	static GameEngineRandom MainRandom;
+
+	// constrcuter destructer
+	GameEngineRandom();
+	~GameEngineRandom();
+
+	// delete Function
+	GameEngineRandom(const GameEngineRandom& _Other) = delete;
+	GameEngineRandom(GameEngineRandom&& _Other) noexcept = delete;
+	GameEngineRandom& operator=(const GameEngineRandom& _Other) = delete;
+	GameEngineRandom& operator=(GameEngineRandom&& _Other) noexcept = delete;
+
+	int RandomInt(int _Min, int _Max)
+	{
+		// 이게 Creater
+		std::uniform_int_distribution<int> Uniform(_Min, _Max);
+		return Uniform(MtGen);
+	}
+
+	float RandomFloat(float _Min, float _Max)
+	{
+		std::uniform_real_distribution <float> Uniform(_Min, _Max);
+		return Uniform(MtGen);
+	}
+
+	void SetSeed(__int64 _Seed)
+	{
+		MtGen = std::mt19937_64(_Seed);
+	}
+
+protected:
+
+
+private:
+	// 메르센 트위스터 알고리즘을 사용한 제네러이터
+	std::mt19937_64 MtGen = std::mt19937_64();
+	// 랜덤 크리에이터 클래스 => 1개
+};
+
